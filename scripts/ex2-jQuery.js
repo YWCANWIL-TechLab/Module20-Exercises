e6e293eeaba0c40dea7cde6f4a6ababdda23f0b3
unknown <GrahamE@ywcanwil.org>
2023-05-23 14:41:28 -0500

    Exercises for Module 20

---
+++ scripts/ex2-jQuery.js
@@ -0,0 +1,118 @@
+// * jQUERY EXERCISES
+// ! All of the exercises in this file should be done using jQuery
+
+/**
+ * Given a String called newValue, replace the value of the element with an id
+ * of "q1" and replace it with newValue. 
+ * 
+ * @param {String} newValue 
+ */
+function selectProblem1(newValue) {
+    // TODO
+}
+
+
+/**
+ * Given a String called tag representing an HTML tag, select and return all 
+ * elements of the given tag.
+ * 
+ * @param {String} tag 
+ */
+function selectAllByTag(tag) {
+    // TODO
+}
+
+
+/**
+ * Given a String called tag which represents an HTML tag and a String called 
+ * newValue which is a new value to replace the value of an element with, replace
+ * the value of all elements of the given tag with newValue.
+ * 
+ * @param {String} tag 
+ * @param {String} newValue 
+ */
+function replaceAllByTag(tag, newValue) {
+    // TODO
+}
+
+
+/**
+ * Given a String called someClass which represents a class attribute of an HTML
+ * element and a String called newValue which is a value to replace an element's
+ * current value with, replace all elements of a class's value with newValue.
+ * 
+ * @param {String} someClass 
+ * @param {String} newValue 
+ */
+function replaceAllByClass(someClass, newValue) {
+    // TODO
+}
+
+
+/**
+ * Using CSS Properties, hide the p element with an id of q2. I recommend using
+ * the visibility property rather than display, but either will work.
+ */
+function hideQ2() {
+    // TODO
+}
+
+
+/**
+ * Using CSS Properties, make the p element with an id of q3 reappear. I recommend
+ * using the visibility property. Feel free to take a peek at the css file to 
+ * see what it is currently using or what the differences between them are.
+ */
+function showQ3() {
+    // TODO
+}
+
+
+/**
+ * Using CSS Properties, change the style of the element with an id of q4 so 
+ * that the font-size becomes 50px.
+ */
+function styleQ4() {
+    // TODO
+}
+
+
+/**
+ * Using CSS Properties, change the style of the element with an id of q5
+ * so that its color property becomes newColor.
+ * 
+ * @param {String} newColor 
+ */
+function styleQ5(newColor) {
+    // TODO
+}
+
+
+/**
+ * Using CSS Properties, change the style of the element with an id of q6 so
+ * that its padding property becomes newPadding.
+ * 
+ * @param {Number} newPadding 
+ */
+function styleQ6(newPadding) {
+    // TODO
+}
+
+
+/**
+ * Using the DOM, return the parent of the element with an id of q7. You must 
+ * access the parent of q7 through q7 rather than checking what its parent is
+ * and returning it directly.
+ */
+function returnParentOfQ7() {
+    // TODO
+}
+
+
+/**
+ * Using the DOM, remove the element with an id of q8. Return the element you 
+ * removed.
+ */
+function removeQ8() {
+    // TODO
+}
